e6a83edb2833e932422a175a26150d6488adddeb
Davi Luiz <davilucena222@gmail.com>
2022-08-20 19:55:06 +0000

    creating transactions context

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+
+interface Transaction {
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
+interface TransactionContextType {
+  transactions: Transaction[];
+}
+
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
+
+export const TransactionsContext = createContext({} as TransactionContextType);
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function loadTransactions() {
+    const response = await fetch("https://3000-davilucena2-dtmoneyblac-blu493mt8wk.ws-us62.gitpod.io/transactions");
+    const data = await response.json();
+
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
+  return (
+    <TransactionsContext.Provider value={{ transactions }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
